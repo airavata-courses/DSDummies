682c15373c4828e4cc8e060451ff06e730733eec
Prerna Achtani <prernaachtani@Prernas-MacBook-Air.local>
2022-02-06 16:37:23 -0500

    closes #15 - cache service to store AWS data

---
+++ cache/src/test/java/com/distsystems/main/cache/CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distsystems.main.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
