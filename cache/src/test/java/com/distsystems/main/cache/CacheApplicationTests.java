b4093bc0bb39b1e02399dc9425e0513d9e01e3e0
Prerna Achtani <prernaachtani@Prernas-MacBook-Air.local>
2022-02-07 20:29:56 -0500

    testing with default test case

--- cache/src/test/java/com/distsystems/main/cache/CacheApplicationTests.java
+++ cache/src/test/java/com/distsystems/main/cache/CacheApplicationTests.java
@@ -1,32 +1,14 @@
 package com.distsystems.main.cache;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.distsystems.main.WeatherDataCacheApplication;
-import com.distsystems.main.Model.WeatherDataCache;
-import com.distsystems.main.repository.WeatherDataRepository;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = WeatherDataCacheApplication.class)
-
-class CacheApplicationTests {
-	
-	@Autowired
-	WeatherDataRepository weatherDataRepository;
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
 
 	@Test
-	public void dbTestCreate() {
-		WeatherDataCache w = new WeatherDataCache();
-		w.setSlug("IN-2020-10-10-05");
-		w.setResponse("Some response string");
-		w.setAddedOn("added on");
-		weatherDataRepository.save(w);
-		assertNotNull(weatherDataRepository.findBySlugContaining("IN-2020-10-10-05"));
+	void contextLoads() {
 	}
+
 }
