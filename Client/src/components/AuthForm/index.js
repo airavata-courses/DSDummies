852c34316250223b3ec255b881d08d71c64abaac
Parth Kapil <parthkapil@Parths-MacBook-Pro.local>
2022-04-06 23:22:33 -0400

    code validation

--- Client/src/components/AuthForm/index.js
+++ Client/src/components/AuthForm/index.js
@@ -70,7 +70,7 @@ export function AuthForm(props) {
 				/>
 				{active === "signin" && (
 					<HeaderContainer>
-						<HeaderText>Bitch Be Humble!</HeaderText>
+						<HeaderText>Welcome -- Deployed!!</HeaderText>
 						<HeaderText>Back</HeaderText>
 						<SmallText>Please sign-in to continue!</SmallText>
 					</HeaderContainer>
